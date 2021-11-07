f7e6e87b3c168740a9d8b8a3cfb9affc4ddc5f7a
Windows_pc <titenkov.vova@gmail.com>
2021-11-08 02:32:03 +0700

    titenkov windows

--- Save_from_web.py
+++ Save_from_web.py
@@ -3,7 +3,7 @@ import requests
 hd = {
         'User-Agent': 'Mozilla/5.0 (compatible; U; ABrowse 0.6; Syllable) AppleWebKit/420+ (KHTML, like Gecko)'
       }
-def save_rom_www(link):
+def save_from_www(link):
     filename = link.split('/')[-3] + ".jpg"
     print(filename)
     r = requests.get(link, headers=hd, allow_redirects=True)
@@ -11,13 +11,13 @@ def save_rom_www(link):
 
 
 
-# n=3131457
-# while n <= 3131459:
-#     n += 1
-#     link3 = 'http://elib.shpl.ru/pages/' + str(n) + '/zooms/8'
-#     print (n)
-#     print (link3)
-#     save_rom_www(link3)
+n=3131457
+while n <= 3131459:
+    n += 1
+    link3 = 'http://elib.shpl.ru/pages/' + str(n) + '/zooms/8'
+    print (n)
+    print (link3)
+    save_from_www(link3)
 
 
 
